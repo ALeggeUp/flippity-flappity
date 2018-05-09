ba01e0dad63107337937c87fb68f6c91c3901bc2
Stephen Legge <stephen@aleggeup.com>
2018-05-09 01:02:53 -0400

    Remove unused 'MyHomePage'

--- lib/main.dart
+++ lib/main.dart
@@ -17,35 +17,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new RandomWords()
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = new Set<WordPair>();
